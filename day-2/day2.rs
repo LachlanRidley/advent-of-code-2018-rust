539afdbf07e6bf72f5af01b384e248c2df09b01c
Lachlan Ridley <lachie.ridley@gmail.com>
2019-04-20 17:35:08 +1200

    Wrote out pseudocode for day 2

---
+++ day-2/day2.rs
@@ -0,0 +1,14 @@
+fn main() {
+  // int twos = 0
+  // int threes = 0
+
+  // iterate strings
+  //   create map of char -> counts
+  // scan through map values for any 2s
+  //   increment twos
+  // scan through map values for any 3s
+  //   increment threes
+  
+  // let checksum = twos * threes
+  // print checksum
+}
