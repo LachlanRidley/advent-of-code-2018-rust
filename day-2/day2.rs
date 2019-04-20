56b5c895861d99229e5a3e0e699b093e2528ecee
Lachlan Ridley <lachie.ridley@gmail.com>
2019-04-20 17:50:02 +1200

    Made some progress on day2

--- day-2/day2.rs
+++ day-2/day2.rs
@@ -1,13 +1,37 @@
-fn main() {
-  // int twos = 0
-  // int threes = 0
+use std::collections::HashMap;
 
-  // iterate strings
-  //   create map of char -> counts
-  // scan through map values for any 2s
-  //   increment twos
-  // scan through map values for any 3s
-  //   increment threes
+fn main() {
+  let puzzle = "abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab";
+
+  let box_ids: Vec<&str> = puzzle.split(',').collect();
+
+  let mut twos = 0;
+  let mut threes = 0;
+
+  
+
+  for box_id in box_ids {
+    let mut map = HashMap::new();
+    
+    // create map of char -> counts
+    for c in box_id.chars() {
+      let foo = match map.get(&c) {
+        Some(x) => x + 1,
+        None => 1
+      };
+      
+      map.insert(c, foo);
+    }
+
+      // for (key, value) in &map {
+      //   println!("{}: {}", key, value);
+      // }
+    
+    // scan through map values for any 2s
+    //   increment twos
+    // scan through map values for any 3s
+    //   increment threes
+  } 
   
   // let checksum = twos * threes
   // print checksum
